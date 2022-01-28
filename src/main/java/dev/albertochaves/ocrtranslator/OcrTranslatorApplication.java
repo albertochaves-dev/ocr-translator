492f7507ff0e9e789f433a33bf8d20ec4d03f293
alberto.chaves <achavesherreros@altran.com>
2022-01-28 14:43:30 +0100

    Initial commit

---
+++ src/main/java/dev/albertochaves/ocrtranslator/OcrTranslatorApplication.java
@@ -0,0 +1,13 @@
+package dev.albertochaves.ocrtranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/albertochaves/ocrtranslator/OcrTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.albertochaves.ocrtranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
